f1746f13a9e00cd78c5c7d7be853f7e48d46c5ce
Arjun Satish <arjun.satish@gmail.com>
2019-01-01 19:44:11 -0800

    Move go files around
    Signed-off-by: Arjun Satish <arjun.satish@gmail.com>

---
+++ go-sol/sourcer.go
@@ -0,0 +1,66 @@
+package main
+
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	broker := "localhost:9092"
+	group := "agroup"
+	var topics []string
+	topics = append(topics, "_confluent-metrics")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":    broker,
+		"group.id":             group,
+		"session.timeout.ms":   6000,
+		"default.topic.config": kafka.ConfigMap{"auto.offset.reset": "earliest"}})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Consumer %v\n", c)
+
+	err = c.SubscribeTopics(topics, nil)
+
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := c.Poll(100)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("%% Message on %s:\n%s\n",
+					e.TopicPartition, string(e.Value))
+			case kafka.PartitionEOF:
+				fmt.Printf("%% Reached %v\n", e)
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+				run = false
+			default:
+				fmt.Printf("Ignored %v\n", e)
+			}
+		}
+	}
+
+	fmt.Printf("Closing consumer\n")
+	c.Close()
+}
